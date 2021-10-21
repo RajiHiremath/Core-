a1863acd274316db907bcab3607e968ffd9962fe
RajiHiremath <rajeshwari99.xworkz@gamil.com>
2021-10-21 10:54:19 +0530

    files uploded

---
+++ Restrict/Internet.java
@@ -0,0 +1,17 @@
+package com.xworkz.access;
+
+	public class Internet {
+
+		String users;
+		
+		void setName(String name)
+		{
+			this.users=name;
+		}
+		
+		String getName()
+		{
+			return this.users;
+		}
+		
+}
---
+++ Restrict/Intranet.java
@@ -0,0 +1,16 @@
+package com.xworkz.access.restrict;
+
+	public class Intranet {
+		 String user;
+		
+	public void setName(String name)
+	{
+		this.user=name;
+	}
+
+	public String getName()
+	{
+		return this.user;
+	}
+	}
+
