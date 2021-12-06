57034b84599f3c32e8214e173162d598bf2b9366
RajiHiremath <91588020+RajiHiremath@users.noreply.github.com>
2021-12-07 00:55:02 +0530

    Add files via upload

---
+++ Collection/src/com/xworkz/sweet/SweetScammer.java
@@ -0,0 +1,87 @@
+package com.xworkz.sweet;
+import java.util.ArrayList;
+import java.util.Collection;
+public class SweetScammer {
+
+	public static void main(String[] args) {
+		
+		String sweet1="Kaju katli";
+		String sweet2="Kotichur";
+		String sweet3="Ghee Motichur";
+		String sweet4="Besan laddu";
+		String sweet5="Dry fruit Burfi";
+		String sweet6="Kaju anar";
+		String sweet7="Kaju kismiss";
+		String sweet8="kaju pan";
+		String sweet9="kaju masala";
+		String sweet10="White kalakand";
+		String sweet11="Kunda";
+		String sweet12="Jamoon";
+		String sweet13="Dry jamoon";
+		String sweet14="Rasgulla";
+		String sweet15="Rusmallai";
+		String sweet16="Doodh pedha";
+		String sweet17="M sikka";
+		String sweet18="Chocolate cake";
+		String sweet19="Ghee gever";
+		String sweet20="Kaju anjur burfi";
+		String sweet21="Kandi pedha";
+		String sweet22="Dharwad pedha";
+		String sweet23="Green burfi";
+		String sweet24="Orange burfi";
+		String sweet25="White burfi";
+		String sweet26="Mysore pak";
+		String sweet27="Ghee mysorepak";
+		String sweet28="Dink laddu";
+		String sweet29="Ghee soan papdi";
+		String sweet30="Milk soan papdi";
+		String sweet31="Orange soan papdi";
+		String sweet32="Chocolate soan papdi";
+		String sweet33="Besan soan papdi";
+		String sweet34="Coconut soan papdi";
+		String sweet35="Kardantu";
+		String sweet36="Dry fruit halwa";
+		String sweet37="Banana Halwa";
+		String sweet38="Pista Halwa";
+		String sweet39="Badam Halwa";
+		String sweet40="Pineapple Halwa";
+		String sweet41="Chandrakala";
+		String sweet42="Mandige";
+		String sweet43="Champakali";
+		String sweet44="Lal pedha";
+		String sweet45="Gokak kardant";
+		String sweet46="Bhoondi";
+		String sweet47="Cham cham";
+		String sweet48="Kesar pedha";
+		String sweet49="Mava pedha";
+		String sweet50="Kaju Anjeer roll";
+		String sweet51="Kaju pista roll";
+		String sweet52="kaju modak";
+		String sweet53="Coconut burfi";
+		String sweet54="Mathura doodh pedha";
+		String sweet55="jelebi";
+		String sweet56="jahangir";
+		String sweet57="pene";
+		String sweet58="White kaladant";
+		String sweet59="khowva";
+		String sweet60="Kaju anjur burfi";
+		
+		
+		Collection collection=new ArrayList();
+		collection.add(sweet1);
+		collection.add(sweet5);
+		collection.add(sweet19);
+		collection.add(sweet21);
+		collection.add(sweet25);
+		collection.add(sweet14);
+		collection.add(sweet10);
+		collection.add(sweet45);
+		collection.add(sweet50);
+		
+		System.out.println(collection.toString());
+	    System.out.println("Number of sweets names added :" + collection.size());
+		
+
+	}
+
+}
